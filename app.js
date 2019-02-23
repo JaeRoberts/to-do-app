a2e070e251ac98cb37cbb0c4f205c39a1d456b05
Jordyn <jae.roberts.d@gmail.com>
2019-02-23 18:20:20 -0500

    Assignment Simple To-Do Part 2: Complete

--- app.js
+++ app.js
@@ -1,45 +1,64 @@
-function onReady(){
-  const TODOS = [];
+function onReady() {
+  let id = 0;
+  let toDos = [];
   const ADDTODOFORM = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const NEWTODOTEXT = document.getElementById('newToDoText');
-    if (!NEWTODOTEXT.value) {return; }
-    TODOS.push({
+    if (!NEWTODOTEXT.value) {
+      return;
+    }
+    toDos.push({
       title: NEWTODOTEXT.value,
-      complete: false
+      complete: false,
+      id: id
     });
+
+    id++;
+
     NEWTODOTEXT.value = '';
 
     renderTheUI();
   }
 
-function renderTheUI(){
-  const TODOLIST = document.getElementById('toDoList');
-  TODOLIST.textContent = '';
+  function renderTheUI() {
+    const TODOLIST = document.getElementById('toDoList');
+    TODOLIST.textContent = '';
 
-  TODOS.forEach(function(toDo) {
-    const NEWLI = document.createElement('li');
-    const CHECKBOX = document.createElement('input');
-    CHECKBOX.type = "checkbox";
+    toDos.forEach(function(toDo) {
+      const NEWLI = document.createElement('li');
+      const CHECKBOX = document.createElement('input');
+      const DELETEBUTTON = document.createElement('button');
 
-    NEWLI.textContent = toDo.title;
+      CHECKBOX.type = "checkbox";
 
-    TODOLIST.appendChild(NEWLI);
-    NEWLI.appendChild(CHECKBOX);
+      DELETEBUTTON.textContent = "Delete";
 
+      DELETEBUTTON.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+        renderTheUI();
+      });
+
+      NEWLI.textContent = toDo.title;
+
+      TODOLIST.appendChild(NEWLI);
+      NEWLI.appendChild(CHECKBOX);
+      NEWLI.appendChild(DELETEBUTTON);
+
+    });
+  }
+
+  ADDTODOFORM.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
-}
 
-ADDTODOFORM.addEventListener('submit', event => {
-  event.preventDefault();
-  createNewToDo();
-});
-
-renderTheUI();
+  renderTheUI();
 
 }
 
-window.onload = function () {
+window.onload = function() {
   onReady();
 };
